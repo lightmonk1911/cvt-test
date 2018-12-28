5fac53f29523a65c6dc184dec0a9fba4ffd2494e
Igor <lightmonk1911@gmail.com>
2018-12-28 20:04:26 +0400

    refactored

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,29 +1,34 @@
 import React from 'react';
 
+const navItemsList = [
+  { text: 'Профиль', tabName: 'profile' },
+  { text: 'Друзья пользователя', tabName: 'friends' },
+];
+
+const NavItem = ({ text, tabName, changeTab, active }) => (
+  <li className="nav-item">
+    <button
+      type="button"
+      onClick={() => changeTab(tabName)}
+      className={`mx-1 nav-link ${active === tabName ? 'active' : ''}`}
+    >
+      {text}
+    </button>
+  </li>
+);
+
 const Navbar = ({ active, changeTab }) => (
   <nav>
     <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <button
-
-          type="button"
-          onClick={() => changeTab('profile')}
-          className={`mx-1 nav-link ${active === 'profile' ? 'active' : ''}`}
-        >
-          Профиль
-        </button>
-      </li>
-      <li className="nav-item">
-        <button
-          type="button"
-          onClick={() => changeTab('friends')}
-          className={`mx-1 nav-link ${active === 'friends' ? 'active' : ''}`}
-        >
-          Друзья пользователя
-        </button>
-      </li>
+      {navItemsList.map(({ text, tabName }) => (
+        <NavItem
+          text={text}
+          tabName={tabName}
+          changeTab={changeTab}
+          active={active}
+        />
+      ))}
     </ul>
-
   </nav>
 );
 
