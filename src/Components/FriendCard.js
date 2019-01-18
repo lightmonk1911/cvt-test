19617c7f4c0c968a7afa2ca191d7c00d2924e3e5
Igor <lightmonk1911@gmail.com>
2019-01-18 20:18:37 +0400

    refactored

--- src/Components/FriendCard.js
+++ src/Components/FriendCard.js
@@ -1,18 +1,29 @@
+/* eslint-disable import/no-dynamic-require */
+/* eslint-disable global-require */
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const FriendCard = ({ name, online, city }) => (
-  <div>
-    <p>{name}</p>
-    <p>{online ? 'online' : null}</p>
-    <p>{city}</p>
-  </div>
-);
+const FriendCard = ({
+  name, online, city, index,
+}) => {
+  const pathToAvatar = require(`../images/avatars/${index + 1}.jpg`);
+  return (
+    <div className="friend-card">
+      <img src={pathToAvatar} alt="ava" className="small-avatar" />
+      <div className="friend-data">
+        <h3 className="friend-name">{name}</h3>
+        <p className="friend-city">{`г. ${city}`}</p>
+        <small className="friend-online-status">{online ? 'Online' : null}</small>
+      </div>
+    </div>
+  );
+};
 
 FriendCard.propTypes = {
   name: PropTypes.string.isRequired,
   online: PropTypes.bool.isRequired,
   city: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
 };
 
 export default FriendCard;
