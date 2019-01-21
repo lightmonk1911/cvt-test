04d1386fdbe7a2f6de42c55c911e7f93fe8e1767
Igor <lightmonk1911@gmail.com>
2019-01-21 11:03:17 +0400

    default tab

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Main from './Components/Main';
 
 class App extends Component {
   state = {
-    active: 'friends',
+    active: 'profile', // 'profile' or 'friends'
   }
 
   changeTab = (tabName) => {
