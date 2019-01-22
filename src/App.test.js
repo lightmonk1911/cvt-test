617c25831afc1b98a4ee73117798f2c000ce7b12
Igor <lightmonk1911@gmail.com>
2019-01-22 13:52:06 +0400

    init

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,61 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 import App from './App';
+import Main from './Components/Main';
+import Navbar from './Components/Navbar';
 
+const Test = () => <div className="container">Test</div>;
 
-const wrapper = shallow(<App />);
+describe('Tests works in common', () => {
+  const wrapper = shallow(<Test />);
 
+  it('Test has .container', () => {
+    expect(wrapper.contains(<div className="container">Test</div>)).toBe(true);
+  });
 
-test('container exist', () => {
-  expect(wrapper.find('.container')).toExist();
+  it('Test has no class .test', () => {
+    expect(wrapper.find('.test')).toHaveLength(0);
+  });
+});
+
+describe('static App', () => {
+  const wrapper = shallow(<App />);
+
+  it('App has class .container', () => {
+    expect(wrapper.hasClass('container')).toBe(true);
+  });
+
+  it('App state.active is "profile"', () => {
+    expect(wrapper.state('active')).toBe('profile');
+  });
+
+  it('App has header', () => {
+    expect(wrapper.find('header')).toHaveLength(1);
+  });
+
+  it('App has Main', () => {
+    expect(wrapper.find(Main)).toHaveLength(1);
+  });
+
+  it('Main "active" prop is profile', () => {
+    expect(wrapper.find(Main).prop('active')).toBe('profile');
+  });
+
+  it('App has Navbar', () => {
+    expect(wrapper.find(Navbar)).toHaveLength(1);
+  });
+
+  it('Navbar "active" prop is profile', () => {
+    expect(wrapper.find(Navbar).prop('active')).toBe('profile');
+  });
+});
+
+describe('behavior App', () => {
+  const wrapper = shallow(<App />);
+  it('should change active', () => {
+    wrapper.find(Navbar).prop('changeTab')('friends');
+    expect(wrapper.state('active')).toBe('friends');
+    expect(wrapper.find(Navbar).prop('active')).toBe('friends');
+    expect(wrapper.find(Main).prop('active')).toBe('friends');
+  });
 });
