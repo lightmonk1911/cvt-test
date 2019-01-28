647f4f659ac6b9064d7855adb9d9bc280c879644
Igor <lightmonk1911@gmail.com>
2019-01-28 09:15:15 +0400

    modules: true in babel-preset-env

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', { modules: false, targets: '> 0.25%, not dead' }],
+    ['@babel/preset-env', { targets: '> 0.25%, not dead' }],
     ['@babel/preset-react'],
   ],
   plugins: ['@babel/plugin-proposal-class-properties'],
